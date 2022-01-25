7837ea7fbefc246372daa34935bb8fad7ddfe053
Dhruvpatel007 <dhruvdpatel0709@gmail.com>
2022-01-25 14:49:49 +0530

    added sucessfully

---
+++ javascript.js
@@ -0,0 +1,54 @@
+// ------------step-wizard-------------
+$(document).ready(function () {
+    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+        var target = $(e.target);
+    
+        if (target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+   
+    $(".next-step").click(function (e) {
+
+        
+        var active = $('.wizard .nav-tabs li.active');
+        active.next().removeClass('disabled');
+        nextTab(active);
+        // $('.connecting-line').hasClass('w-66').addClass('w-100');
+        // $('.connecting-line').hasClass('w-33').addClass('w-66');
+        
+        
+        
+        
+        
+
+
+
+        
+    });
+    // $(".prev-step").click(function (e) {
+
+    //     var active = $('.wizard .nav-tabs li.active');
+    //     prevTab(active);
+        
+    // });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+// function prevTab(elem) {
+//     $(elem).prev().find('a[data-toggle="tab"]').click();
+// }
+
+
+$('.nav-tabs').on('click', 'li', function() {
+    /*$('.nav-tabs li.active').removeClass('active');*/
+    $(this).addClass('active');
+});
+
+
